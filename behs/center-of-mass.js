90c5db043a1057e0092359d40e120fc2f81d2abc
Lise Ho <koprty@users.noreply.github.com>
2017-08-05 17:24:45 -0400

    refactor for line colors and strokes

--- behs/center-of-mass.js
+++ behs/center-of-mass.js
@@ -16,12 +16,16 @@ import * as Display from 'display';
 const pb = new P5Behavior();
 
 const COLORS = {
-  RED: [255, 0, 0]
+  RED: [255, 0, 0],
+  GREEN: [0, 255, 0],
+  BLUE: [0, 0, 255],
+  GRAY: [155, 155, 155]
 };
 
 const CENTER_RADIUS = 20;
 const GOAL_RADIUS = 10;
 const goalX, goalY = parseInt(Math.random() * Display.width);
+const MASS_CONNECTORS_STROKE_WEIGHT = 4;
 
 /** Helper Functions **/
 const drawCircle = function(x, y, r, color) {
@@ -31,9 +35,25 @@ const drawCircle = function(x, y, r, color) {
   this.ellipse(x, y, r * 2, r * 2);
 };
 
+const drawLine = function(x1, y1, x2, y2) {
+    this.line(x1, y1, x2, y2);
+};
+const drawCenterMassConnectors = function (x1, y1, x2, y2) {
+    this.strokeWeight(MASS_CONNECTORS_STROKE_WEIGHT);
+    this.stroke(COLORS.GREEN);
+
+    this.drawLine(x1, y1, x2, y2);
+
+    // Reset defaults for stroke colors.
+    this.strokeWeight(1);
+    this.stroke(COLORS.GRAY);
+};
+
 /** Lifecycle Functions **/
 pb.setup = function(p) {
   this.drawCircle = drawCircle;
+  this.drawLine = drawLine;
+  this.drawCenterMassConnectors = drawCenterMassConnectors;
   goalX = parseInt(Math.random() * Display.width);
   goalY = parseInt(Math.random() * Display.height);
 };
@@ -51,7 +71,7 @@ pb.draw = function(floor, p) {
   centerY /= numUsers;
   this.drawCircle(centerX, centerY, CENTER_RADIUS);
   for (let user of floor.users) {
-    this.line(user.x, user.y, centerX, centerY);
+    this.drawCenterMassConnectors(user.x, user.y, centerX, centerY);
   }
 
   this.drawCircle(goalX, goalY, GOAL_RADIUS, COLORS.RED);
